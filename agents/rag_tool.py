eb91dc471d2941aed9cc6c5058f11bebb1e8a795
Mutahir-Ahmed <mutahirahmedc745@gmail.com>
2026-06-23 20:52:23 +0500

    rag_tool.py

--- agents/rag_tool.py
+++ agents/rag_tool.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from langchain_core.documents import Document
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.tools import Tool
 
 # ──────────────────────────────────────────────────────────────────────────────
